7d59d451559a2012d44a92a7e0cdd975f7bf7d48
SorEgo <146003878+SwornEgo@users.noreply.github.com>
2025-10-16 18:29:33 -0400

    Implemented CachingBreedFetcher

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -16,6 +16,7 @@ import java.util.*;
  */
 public class DogApiBreedFetcher implements BreedFetcher {
     private final OkHttpClient client = new OkHttpClient();
+    private static final String API_URL = "https://dog.ceo/api";
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
@@ -24,12 +25,44 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws RuntimeException {
         // TODO Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
+        List<String> subBreeds = new ArrayList<>();
+
+        final Request request = new Request.Builder()
+                .url(String.format("%s/breed/%s/list", API_URL, breed))
+                .build();
+
+        try {
+            final Response response = client.newCall(request).execute();
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if  (response.isSuccessful()) {
+                final JSONArray subBreedsJSON = (JSONArray) responseBody.get("message");
+
+                for  (int i = 0; i < subBreedsJSON.length(); i++) {
+                    subBreeds.add((String) subBreedsJSON.get(i));
+                }
+            }
+            else {
+                throw new BreedNotFoundException("Dog breed not found");
+            }
+
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        return subBreeds;
     }
+
+//    public static void main(String[] args) {
+//        DogApiBreedFetcher breedFetcher = new DogApiBreedFetcher();
+//        System.out.println(breedFetcher.getSubBreeds("Hound"));
+//    }
 }
+
